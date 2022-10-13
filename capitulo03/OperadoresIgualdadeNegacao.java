082bd8241c4060dc3a9597698d55e5f3fb996b36
fabio <fabiuhp@msn.com>
2022-10-13 18:58:13 -0300

    operadores

---
+++ capitulo03/OperadoresIgualdadeNegacao.java
@@ -0,0 +1,8 @@
+public class OperadoresIgualdadeNegaocao {
+  public static void main(String[] args) {
+//      boolean numerosIguais = 10 == 10;
+      boolean numerosIguais = (4+6) == (5*2);
+
+      System.out.println(numerosIguais);
+  }
+}
