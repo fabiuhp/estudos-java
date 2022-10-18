bc53d648860382c4718b1741270d991e0df74dfa
fabio <fabiuhp@msn.com>
2022-10-17 22:12:00 -0300

    operadores de comparacao

---
+++ capitulo03/ExameCortisol.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExameCortisol {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cortisol: ");
+        double cortisol = sc.nextDouble();
+
+        // Cortisol ideal é de 6 a 18.4
+        // Uso de operadores de comparação && ou || (and/or)
+        boolean resultadoNormal = cortisol  >= 6 && cortisol <= 18.4;
+
+        System.out.printf("Cortisol está normal? %b%n", resultadoNormal);
+    }
+}
