9711c19b6e66859d3087054af463f7037819dfaa
fabio <fabiuhp@msn.com>
2022-11-01 11:27:15 -0300

    logica da revenda

--- capitulo05/Carro.java
+++ capitulo05/Carro.java
@@ -5,9 +5,20 @@ public class Carro {
     String modelo;
     String cor;
     int anoFabricacao;
+    double precoCompra;
     Pessoa pessoa;
 
     void calcularValorRevenda() {
+        int vidaUtilEmAnos = 20;
+        int tempoDeUsoEmAnos = 2022 - anoFabricacao;
+        double valorRevenda = (precoCompra / vidaUtilEmAnos) * (vidaUtilEmAnos - tempoDeUsoEmAnos);
+        
+        if (valorRevenda < 0) {
+            valorRevenda = 0;
+        }
+
         System.out.printf("Calcular valor de revenda de: %s %d%n", modelo, anoFabricacao);
+        System.out.printf("Tempo de uso em anos: %d%n", tempoDeUsoEmAnos);
+        System.out.printf("Valor revenda: %6.2f%n", valorRevenda);
     }
 }
--- capitulo05/Principal.java
+++ capitulo05/Principal.java
@@ -2,16 +2,26 @@ package capitulo05;
 
 public class Principal {
     public static void main(String[] args) {
-        Carro meuCarro = new Carro();
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
         Pessoa fabio = new Pessoa();
         fabio.nome = "Fabio";
 
-        meuCarro.cor = "Branco";
-        meuCarro.anoFabricacao = 2022;
-        meuCarro.fabricante = "Fiat";
-        meuCarro.modelo = "Pulse";
-        meuCarro.pessoa = fabio;
+        carro1.cor = "Branco";
+        carro1.anoFabricacao = 2021;
+        carro1.fabricante = "Fiat";
+        carro1.modelo = "Pulse";
+        carro1.pessoa = fabio;
+        carro1.precoCompra = 120000;
 
-        meuCarro.calcularValorRevenda();
+        carro2.cor = "Azul";
+        carro2.anoFabricacao = 2022;
+        carro2.fabricante = "BMW";
+        carro2.modelo = "X1";
+        carro2.pessoa = fabio;
+        carro2.precoCompra = 200000;
+
+        carro1.calcularValorRevenda();
+        carro2.calcularValorRevenda();
     }
 }
