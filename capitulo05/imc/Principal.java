9dd61529e2d51e47ac6ec7c6db79537ff1acd37d
fabio <fabiuhp@msn.com>
2022-11-15 21:19:15 -0300

    fix na principal

--- capitulo05/imc/Principal.java
+++ capitulo05/imc/Principal.java
@@ -3,10 +3,8 @@ package capitulo05.imc;
 public class Principal {
     public static void main(String[] args) {
         CalculadoraImc calculadoraImc = new CalculadoraImc();
-        calculadoraImc.altura = 1.82;
-        calculadoraImc.peso = 175;
 
-        IndiceMassaCorporal imc = calculadoraImc.calcular();
+        IndiceMassaCorporal imc = calculadoraImc.calcular(175, 1.82);
 
         if (imc.estaObeso()) {
             System.out.println("Obeso");
