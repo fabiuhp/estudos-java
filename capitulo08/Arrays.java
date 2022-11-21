f11b613c2f023c03f873072bf56de8e3c9a9e572
fabio <fabiuhp@msn.com>
2022-11-21 19:32:36 -0300

    capitulo de arrays

---
+++ capitulo08/Arrays.java
@@ -0,0 +1,13 @@
+package capitulo08;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //algumas maneiras de declarar arrays
+        // int[] notas = new int[5];
+        // int[] notas;
+        // notas = new int[]{1,2,3,4,5};
+        // int[] notas = new int[]{1,2,3,4,5};
+        // int[] notas = {1,2,3,4,5};
+        // int []notas = new int[5];
+    }
+}
