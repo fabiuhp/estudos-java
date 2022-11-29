1bf885e53182b6eb6df7892b26231e0292b68c33
fabio <fabiuhp@msn.com>
2022-11-29 17:11:15 -0300

    heranca implementada

--- capitulo12/banco/ContaEspecial.java
+++ capitulo12/banco/ContaEspecial.java
@@ -1,50 +1,12 @@
 package capitulo12.banco;
 
-public class ContaEspecial {
+public class ContaEspecial extends ContaInvestimento {
     
-    private Titular titular;
-    private int agencia;
-    private int numero;
-    private double saldo;
-
-    private double valorTotalRendimentos;
     private double tarifaMensal;
     private double limiteChequeEspecial;
 
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public Titular getTitular() {
-        return titular;
-    }
-
-    public void setTitular(Titular titular) {
-        this.titular = titular;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
     public double getSaldoDisponível() {
         return getSaldo() + getLimiteChequeEspecial();
-    }
-
-    public double getValorTotalRendimentos() {
-        return valorTotalRendimentos;
     }
 
     public double getTarifaMensal() {
@@ -61,33 +23,6 @@ public class ContaEspecial {
 
     public void setLimiteChequeEspecial(double limiteChequeEspecial) {
         this.limiteChequeEspecial = limiteChequeEspecial;
-    }
-
-    public void creditarRendimentos(double percentualJuros) {
-        double valorRendimentos = getSaldo() * percentualJuros / 100;
-        valorTotalRendimentos += valorRendimentos;
-        
-        depositar(valorRendimentos);
-    }
-
-    public void sacar(double valorSaque) {
-        if (valorSaque <= 0) {
-            throw new IllegalArgumentException("Valor deve ser maior que zero.");
-        }
-
-        if (getSaldoDisponível() < getSaldo()) {
-            throw new IllegalArgumentException("Saldo insuficiente para saque.");
-        }
-
-        saldo -= valorSaque;
-    }
-
-    public void depositar(double valorDeposito) {
-        if (valorDeposito <= 0) {
-            throw new IllegalArgumentException("Valor deve ser maior que zero.");
-        }
-
-        saldo += valorDeposito;
     }
 
     public void debitarTarifaMensal() {
--- capitulo12/banco/ContaInvestimento.java
+++ capitulo12/banco/ContaInvestimento.java
@@ -1,41 +1,8 @@
 package capitulo12.banco;
 
-public class ContaInvestimento {
+public class ContaInvestimento extends Conta {
     
-    private Titular titular;
-    private int agencia;
-    private int numero;
-    private double saldo;
-
     private double valorTotalRendimentos;
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public Titular getTitular() {
-        return titular;
-    }
-
-    public void setTitular(Titular titular) {
-        this.titular = titular;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 
     public double getValorTotalRendimentos() {
         return valorTotalRendimentos;
@@ -46,33 +13,5 @@ public class ContaInvestimento {
         valorTotalRendimentos += valorRendimentos;
         
         depositar(valorRendimentos);
-    }
-
-    public void sacar(double valorSaque) {
-        if (valorSaque <= 0) {
-            throw new IllegalArgumentException("Valor deve ser maior que zero.");
-        }
-
-        if (getSaldo() < valorSaque) {
-            throw new IllegalArgumentException("Saldo insuficiente para saque.");
-        }
-
-        saldo -= valorSaque;
-    }
-
-    public void depositar(double valorDeposito) {
-        if (valorDeposito <= 0) {
-            throw new IllegalArgumentException("Valor deve ser maior que zero.");
-        }
-
-        saldo += valorDeposito;
-    }
-
-    public void imprimirDemonstrativo() {
-        System.out.println();
-        System.out.printf("Agência: %d%n", getAgencia());
-        System.out.printf("Conta: %d%n", getNumero());
-        System.out.printf("Titular: %s%n", getTitular().getNome());
-        System.out.printf("Saldo: %.2f%n", getSaldo());
     }
 }
