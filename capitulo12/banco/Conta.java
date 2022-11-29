8a991c4b5028f45449538261e36c8fc216e68942
fabio <fabiuhp@msn.com>
2022-11-28 21:25:03 -0300

    projeto cap12

---
+++ capitulo12/banco/Conta.java
@@ -0,0 +1,65 @@
+package capitulo12.banco;
+
+public class Conta {
+    
+    private Titular titular;
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque <= 0) {
+            throw new IllegalArgumentException("Valor deve ser maior que zero.");
+        }
+
+        if (valorSaque > getSaldo()) {
+            throw new IllegalArgumentException("Saldo insuficiente para saque.");
+        }
+
+        saldo -= valorSaque;
+    }
+
+    public void depositar(double valorDeposito) {
+        if (valorDeposito <= 0) {
+            throw new IllegalArgumentException("Valor deve ser maior que zero.");
+        }
+
+        saldo += valorDeposito;
+    }
+
+    public void imprimirDemonstrativo() {
+        System.out.println();
+        System.out.printf("Agência: %d%n", getAgencia());
+        System.out.printf("Conta: %d%n", getNumero());
+        System.out.printf("Titular: %s%n", getTitular().getNome());
+        System.out.printf("Saldo: %.2f%n", getSaldo());
+    }
+}
