7656303b62c40b6e20750b5df6b12cd3b5eac71a
fabio <fabiuhp@msn.com>
2022-11-28 22:10:06 -0300

    sem heranca

--- capitulo12/Principal2.java
+++ capitulo12/Principal2.java
@@ -3,18 +3,24 @@ package capitulo12;
 import capitulo12.banco.Conta;
 import capitulo12.banco.Titular;
 
-public class Principal {
+public class Principal2 {
     public static void main(String[] args) {
         
         Conta conta1 = new Conta();
         conta1.setTitular(new Titular("Fabio", "12345"));
         conta1.setAgencia(1234);
         conta1.setNumero(1111);
+        conta1.setTipo(Conta.ESPECIAL);
+        conta1.setLimiteChequeEspecial(100);
+        conta1.setTarifaMensal(10);
 
-        conta1.imprimirDemonstrativo();
         conta1.depositar(100);
         conta1.imprimirDemonstrativo();
-        conta1.sacar(10);
-        conta1.imprimirDemonstrativo();    
+        conta1.creditarRendimentos(10);
+        conta1.imprimirDemonstrativo();
+
+        conta1.sacar(200);
+        conta1.debitarTarifaMensal();
+        conta1.imprimirDemonstrativo();
     }
 }
--- capitulo12/banco/Conta.java
+++ capitulo12/banco/Conta.java
@@ -1,11 +1,23 @@
 package capitulo12.banco;
 
 public class Conta {
+
+    public static final int NORMAL = 0;
+    public static final int INVESTIMENTO = 1;
+    public static final int ESPECIAL = 2;
     
     private Titular titular;
     private int agencia;
     private int numero;
     private double saldo;
+    private int tipo = NORMAL;
+
+    //conta investimento
+    private double valorTotalRendimentos;
+
+    //conta especial
+    private double tarifaMensal;
+    private double limiteChequeEspecial;
 
     public int getAgencia() {
         return agencia;
@@ -35,12 +47,66 @@ public class Conta {
         return saldo;
     }
 
+    public int getTipo() {
+        return tipo;
+    }
+
+    public double getSaldoDisponível() {
+        return getSaldo() + getLimiteChequeEspecial();
+    }
+
+    public void setTipo(int tipo) {
+        if (tipo > 2 && tipo < 0) {
+            throw new IllegalArgumentException("Tipo inválido.");
+        }
+
+        this.tipo = tipo;
+
+        if (this.tipo != ESPECIAL) {
+            this.limiteChequeEspecial = 0;
+        }
+    }
+
+    public double getValorTotalRendimentos() {
+        return valorTotalRendimentos;
+    }
+
+    public double getTarifaMensal() {
+        return tarifaMensal;
+    }
+
+    public void setTarifaMensal(double tarifaMensal) {
+        this.tarifaMensal = tarifaMensal;
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        if (this.tipo != ESPECIAL) {
+            throw new RuntimeException("Esta conta não permite cheque especial.");
+        }
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    public void creditarRendimentos(double percentualJuros) {
+        if (getTipo() == NORMAL) {
+            throw new RuntimeException("Conta normal não tem rendimentos");
+        }
+
+        double valorRendimentos = getSaldo() * percentualJuros / 100;
+        valorTotalRendimentos += valorRendimentos;
+        
+        depositar(valorRendimentos);
+    }
+
     public void sacar(double valorSaque) {
         if (valorSaque <= 0) {
             throw new IllegalArgumentException("Valor deve ser maior que zero.");
         }
 
-        if (valorSaque > getSaldo()) {
+        if (getSaldoDisponível() < getSaldo()) {
             throw new IllegalArgumentException("Saldo insuficiente para saque.");
         }
 
@@ -55,11 +121,20 @@ public class Conta {
         saldo += valorDeposito;
     }
 
+    public void debitarTarifaMensal() {
+        if (getTipo() == ESPECIAL) {
+            sacar(getTarifaMensal());
+        } else {
+            throw new IllegalArgumentException("Conta sem tarifa mensal.");
+        }
+    }
+
     public void imprimirDemonstrativo() {
         System.out.println();
         System.out.printf("Agência: %d%n", getAgencia());
         System.out.printf("Conta: %d%n", getNumero());
         System.out.printf("Titular: %s%n", getTitular().getNome());
         System.out.printf("Saldo: %.2f%n", getSaldo());
+        System.out.printf("Saldo disponível: %.2f%n", getSaldoDisponível());
     }
 }
