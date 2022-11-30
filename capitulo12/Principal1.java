c8cf4c386f3721f1ea4e4cd3841a003cbec52c3b
fabio <fabiuhp@msn.com>
2022-11-29 21:48:00 -0300

    alguns refats

--- capitulo12/Principal1.java
+++ capitulo12/Principal1.java
@@ -1,20 +1,17 @@
 package capitulo12;
 
-import capitulo12.banco.Conta;
+import capitulo12.banco.ContaEspecial;
 import capitulo12.banco.Titular;
 
 public class Principal1 {
     public static void main(String[] args) {
         
-        Conta conta1 = new Conta();
+        ContaEspecial conta1 = new ContaEspecial();
         conta1.setTitular(new Titular("Fabio", "12345"));
         conta1.setAgencia(1234);
         conta1.setNumero(1111);
-
-        conta1.imprimirDemonstrativo();
         conta1.depositar(100);
+        conta1.creditarRendimentos(10);
         conta1.imprimirDemonstrativo();
-        conta1.sacar(10);
-        conta1.imprimirDemonstrativo();    
     }
 }
