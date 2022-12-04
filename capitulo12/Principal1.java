8686b9dc981deb0a3f28e1c918a9f1d8ef9e455a
fabio <fabiuhp@msn.com>
2022-12-04 14:56:20 -0300

    upcasting

--- capitulo12/Principal1.java
+++ capitulo12/Principal1.java
@@ -1,13 +1,19 @@
 package capitulo12;
 
 import capitulo12.banco.Conta;
+import capitulo12.banco.ContaEspecial;
 import capitulo12.banco.Titular;
 
 public class Principal1 {
     public static void main(String[] args) {
         
-        Conta conta1 = new Conta(new Titular("Fabio", "12345"), 1234, 1111);
+        ContaEspecial conta1 = new ContaEspecial(new Titular("Fabio", "12345"), 1234, 1111, 10);
         conta1.depositar(100);
         conta1.imprimirDemonstrativo();
+
+
+       //Upcasting de referencias 
+        // Conta conta = (Conta) conta1;
+        // Conta conta = conta1;
     }
 }
