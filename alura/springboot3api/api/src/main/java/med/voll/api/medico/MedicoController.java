3e485999b97d67166d56e3abd4b19bd8be9f7256
fabio <fabiuhp@msn.com>
2022-12-27 18:24:50 -0300

    controller paciente e medico

---
+++ alura/springboot3api/api/src/main/java/med/voll/api/medico/MedicoController.java
@@ -0,0 +1,16 @@
+package med.voll.api.medico;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medico")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
+    }
+}
