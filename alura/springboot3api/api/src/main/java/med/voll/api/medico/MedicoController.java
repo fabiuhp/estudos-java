2f788bb02c635f17bde4afc9eb8a0574b198571c
fabio <fabiuhp@msn.com>
2022-12-27 21:48:49 -0300

    migration de medico

--- alura/springboot3api/api/src/main/java/med/voll/api/medico/MedicoController.java
+++ alura/springboot3api/api/src/main/java/med/voll/api/medico/MedicoController.java
@@ -1,5 +1,7 @@
 package med.voll.api.medico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medico")
 public class MedicoController {
 
+    private final MedicoRepository medicoRepository;
+
+    @Autowired
+    public MedicoController(MedicoRepository medicoRepository) {
+        this.medicoRepository = medicoRepository;
+    }
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        medicoRepository.save(new Medico(dados));
     }
 }
