0b3b80f18e909f29c15587ed22c2951730939105
fabio <fabiuhp@msn.com>
2022-12-28 04:10:29 -0300

    BeanValidation

--- alura/springboot3api/api/src/main/java/med/voll/api/medico/MedicoController.java
+++ alura/springboot3api/api/src/main/java/med/voll/api/medico/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
     }
 }
