2abc30eaf3296cb82b1cf0131238c960f4c82f39
fabio <fabiuhp@msn.com>
2022-12-28 18:15:44 -0300

    persistencia de dados de Paciente

--- alura/springboot3api/api/src/main/java/med/voll/api/paciente/PacienteController.java
+++ alura/springboot3api/api/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -1,5 +1,8 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("paciente")
 public class PacienteController {
 
+    private final PacienteRepository pacienteRepository;
+
+    @Autowired
+    public PacienteController(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        pacienteRepository.save(new Paciente(dados));
     }
 }
---
+++ alura/springboot3api/api/src/main/resources/db/migration/V2__inserirTelefoneEmMedico.sql
@@ -0,0 +1,1 @@
+ALTER TABLE medico ADD COLUMN telefone varchar(20) not null;
