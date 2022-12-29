3aa2bb4ebc4197e0bac1f39f4e41855f56e165f2
fabio <fabiuhp@msn.com>
2022-12-28 22:22:45 -0300

    remocao import

--- alura/springboot3api/api/src/main/java/med/voll/api/paciente/PacienteRepository.java
+++ alura/springboot3api/api/src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -1,6 +1,5 @@
 package med.voll.api.paciente;
 
-import med.voll.api.medico.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
